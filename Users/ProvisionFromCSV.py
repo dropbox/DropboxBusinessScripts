96e8ba4d7293ce412c5370a2b1988c49ff854b62
Kevin Jordan <kevin.jordan@hothouse.com.au>
2018-01-15 15:42:03 +0000

    Update ProvisionFromCSV.py to API v2

--- Users/ProvisionFromCSV.py
+++ Users/ProvisionFromCSV.py
@@ -6,17 +6,17 @@ import argparse
 
 # Look up a member id from an email address
 def getMemberId(token, email):
-    lookupRequest = urllib2.Request('https://api.dropbox.com/1/team/members/get_info', json.dumps({'email': email }))
+    lookupRequest = urllib2.Request('https://api.dropboxapi.com/2/team/members/get_info', json.dumps({'members':[{'.tag':'email','email': email}] }))
     lookupRequest.add_header("Authorization", "Bearer "+token)
     lookupRequest.add_header("Content-type", 'application/json')
     profile = json.loads(urllib2.urlopen(lookupRequest).read())
-    return  profile['profile']['member_id']
+    return  profile[0]['profile']['team_member_id']
 
 # Give an member admin permissions
 def grantAdmin(token, memberId):
     if memberId is None:
         return
-    addRequest = urllib2.Request('https://api.dropbox.com/1/team/members/set_permissions', json.dumps({'member_id': memberId, 'new_is_admin':True }))
+    addRequest = urllib2.Request('https://api.dropboxapi.com/2/team/members/set_admin_permissions', json.dumps({'user': { '.tag': 'team_member_id', 'team_member_id': memberId}, 'new_role':'team_admin' }))
     addRequest.add_header("Authorization", "Bearer "+token)
     addRequest.add_header("Content-type", 'application/json')
     urllib2.urlopen(addRequest);
@@ -27,7 +27,7 @@ def grantAdmin(token, memberId):
 parser = argparse.ArgumentParser(description='Invites Dropbox for Business members from a CSV file.')
 parser.add_argument('file', type=argparse.FileType('r'), help='CSV File of users to provision. Format is [email,firstname,lastname]')
 parser.add_argument( '-s', '--silent', action='store_const', const=True, default=False, dest='silent', help='Silent invite. Skips welcome email.')
-parser.add_argument( '-a', '--admin', action='store_const', const=True, default=False, dest='admin', help='Grants admin permissions to invited members.')
+parser.add_argument( '-a', '--admin', action='store_const', const=True, default=False, dest='admin', help='Grants Team Admin permissions to invited members.')
 
 args = parser.parse_args()
 
@@ -50,15 +50,37 @@ for row in csvreader:
         
     # Add the member
     welcome = not args.silent
-    addRequest = urllib2.Request('https://api.dropbox.com/1/team/members/add', json.dumps({'member_email': row[0], 'member_given_name': row[1], 'member_surname': row[2], 'send_welcome_email':welcome}))
+    addRequest = urllib2.Request('https://api.dropboxapi.com/2/team/members/add', json.dumps({ 'new_members':[{ 'member_email': row[0], 'member_given_name': row[1], 'member_surname': row[2], 'send_welcome_email':welcome}], 'force_async': False}))
     addRequest.add_header("Authorization", "Bearer "+dfbToken)
     addRequest.add_header("Content-type", 'application/json')
     
     try:
         print "  inviting "+row[0]
         profile = json.loads(urllib2.urlopen(addRequest).read())
-        if args.admin:        
-            grantAdmin(token=dfbToken, memberId=profile['profile']['member_id'])
+
+        aTeamMemberID = 0
+
+        # If the member has already been invited        
+        if ( profile['complete'][0]['.tag'] == 'user_already_on_team'):
+            print "    user is already invited to the DfB team"
+
+            member_id = getMemberId(token=dfbToken, email=row[0])
+            aTeamMemberID = member_id
+    
+            # re-send the invite if we're not in silent mode
+            if not args.silent:
+                welcomeRequest = urllib2.Request('https://api.dropboxapi.com/2/team/members/send_welcome_email', json.dumps({'.tag': 'team_member_id','team_member_id': member_id }))
+                welcomeRequest.add_header("Authorization", "Bearer "+dfbToken)
+                welcomeRequest.add_header("Content-type", 'application/json')
+                urllib2.urlopen(welcomeRequest).read()
+                print "    re-sent invitation"
+                
+        else:        
+            aTeamMemberID = profile['complete'][0]['profile']['team_member_id']
+
+        # Apply Team Admin permissions if required
+        if args.admin:       
+            grantAdmin(token=dfbToken, memberId=aTeamMemberID)
         
     except urllib2.HTTPError, error:
         msg = json.loads(error.read())["error"]        
@@ -67,25 +89,5 @@ for row in csvreader:
         if error.code == 401 or error.code == 403:
             parser.error(msg)
 
-        # If the member has already been invited        
-        if (msg == 'user_already_on_team'):
-            print "    user is already invited to the DfB team"
-            
-            member_id = getMemberId(token=dfbToken, email=row[0])
-                
-            # re-send the invite if we're not in silent mode
-            if not args.silent:
-                welcomeRequest = urllib2.Request('https://api.dropbox.com/1/team/members/send_welcome_email', json.dumps({'member_id': member_id }))
-                welcomeRequest.add_header("Authorization", "Bearer "+dfbToken)
-                welcomeRequest.add_header("Content-type", 'application/json')
-                urllib2.urlopen(welcomeRequest).read()
-                print "    re-sent invitation"
-
-            # grant the admin
-            if args.admin:
-                grantAdmin(token=dfbToken, memberId=member_id)
-                
-        else:        
-            print "    error: "+msg
 
 print "Done"
